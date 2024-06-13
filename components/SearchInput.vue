d769592236206e125076ce9e7c2fcb2f5797b95c
nethris <david.abou@epitech.eu>
2024-06-13 09:40:39 -0400

    chore(lint): prettier search input

--- components/SearchInput.vue
+++ components/SearchInput.vue
@@ -5,8 +5,9 @@
     Search documentation...
     <kbd
       class="flex items-center h-5 bg-white leading-5 rounded-md px-1.5 text-xs text-neutral-900 font-medium ring-1 ring-neutral-200"
-      >Ctrl K</kbd
     >
+      Ctrl K
+    </kbd>
   </button>
 </template>
 
